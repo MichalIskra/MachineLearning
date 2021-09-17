db23e210fa8116cd039f77a21ee37a3a7011f66e
MichalIskra <74827725+MichalIskra@users.noreply.github.com>
2021-09-17 22:57:23 +0200

    Upgraded CNN architecture - better results

--- CNN_Cifar10_Keras.py
+++ CNN_Cifar10_Keras.py
@@ -1,53 +1,55 @@
-import tensorflow
-from tensorflow import keras
+import numpy as np
+import tensorflow as tf
 
-
-from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization
-from keras.losses import SparseCategoricalCrossentropy
+import keras
 from keras.models import Sequential
-from livelossplot import PlotLossesKeras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Conv2D, MaxPool2D, Dropout, GlobalAveragePooling2D, BatchNormalization, Flatten
+from keras.datasets import cifar10
+from keras.models import Model
+print(tf.__version__)
+
+tf.test.is_gpu_available(
+    cuda_only=False, min_cuda_compute_capability=None
+)
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+aug_train = ImageDataGenerator(
+            rotation_range=15,
+
+            height_shift_range=0.1,
+            horizontal_flip=True,
+            zoom_range=[0.5,1.0],
+            rescale=1./255,
+            width_shift_range=0.1,)
+
+aug_test = ImageDataGenerator(rescale=1./255)
 
 
-#import datasets
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-
-
-#Checking loading
-assert x_train.shape == (50000, 32, 32, 3)
-assert x_test.shape == (10000, 32, 32, 3)
-assert y_train.shape == (50000, 1)
-assert y_test.shape == (10000, 1)
-
-
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-
-
-num_classes = 10
-
-
-#model architecture
 model = Sequential()
 
-model.add(Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(32, 32, 3)))
-model.add(BatchNormalization())
-model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(64, (5, 5), padding='same', activation='relu', input_shape=(32, 32, 3)))
 model.add(BatchNormalization())
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D((2, 2)))
+model.add(MaxPool2D((2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
 model.add(BatchNormalization())
 model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
 model.add(BatchNormalization())
-model.add(MaxPooling2D((2, 2)))
+model.add(MaxPool2D((2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
@@ -55,24 +57,14 @@ model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.1))
 model.add(Dense(256, activation='relu'))
 
-model.add(Dense(10))
+model.add(Dense(10,activation = 'softmax'))
 
-
-
-model.compile(optimizer='adam',
-              loss=SparseCategoricalCrossentropy(from_logits=True),
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
               metrics=['accuracy'])
 
-#model training
+training_batch = 128
+test_batch = 32
 
-model.fit(x_train, y_train,
-          epochs=15,
-          validation_split=0.1,
-          callbacks=[PlotLossesKeras()],
-          verbose=0)
-
-
-print(model.summary())
-
-score = model.evaluate(x_test, y_test, verbose=2)
-print(score)
+hist = model.fit(aug_train.flow(x_train, y_train, batch_size=training_batch), epochs=80,
+                 validation_data=aug_test.flow(x_test, y_test, batch_size=test_batch))
